e850c3e36aa69f6c2b641457819f3d931dd84ee9
Josh Machado <joshmachado@Joshs-MacBook-Air.local>
2022-12-17 11:33:14 +0530

    Updated Subsequence

--- Array Problems/longest_common_subseq.py
+++ Array Problems/longest_common_subseq.py
@@ -1,14 +1,12 @@
 def longestCommonSubsequence(text1: str, text2: str) -> int:
-        text = [(a, i) for a, i in enumerate(text2)]
-        count = 0
-        
-        if any(text[]) in text1:
-            count+=1
-            for i in range(0, len(text) - 1):
-                if text[i+1] in text1.split(text[i])[1]:
-                    count+=1
-                    
-                print(count)
+        common = [i for i in text1 if i in text2]
+
+        while True:    
+            for i in range(0,len(common)):
+                
+                
+        return count
+
 
         return count
 
